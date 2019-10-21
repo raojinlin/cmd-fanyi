2e8effceeb11f7d519fddd7795c50e7db148a3ca
raojinlin <1239015423@qq.com>
2019-10-21 23:43:18 +0800

    add encoding

--- translate/abstratRequest.py
+++ translate/abstratRequest.py
@@ -1,6 +1,8 @@
+# encoding: utf-8
+
+
 class AbstractRequest(object):
-    “”“抽象翻译请求
-    ”“”
+    """抽象翻译请求"""
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
                  "Chrome/73.0.3683.86 Safari/537.36 "
 
